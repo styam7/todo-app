f653810da4dbde94bbb08ef9af1a2388cab8b704
styam7 <styam.7@gmail.com>
2023-03-05 23:40:25 +0530

    input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import InputField from './components/InputField';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <div className='heading'>Taskify</div>
+      <InputField />
     </div>
   );
 }
