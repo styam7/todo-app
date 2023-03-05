64b9ca5d1d210b3cdeb2265f3eb0c0e85086dd49
styam7 <styam.7@gmail.com>
2023-03-05 23:51:17 +0530

    create interface for passing props

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import './style.css'
 
-const InputField = () => {
+interface props {
+    todo: string,
+    setTodo: React.Dispatch<React.SetStateAction<string>>
+}
+
+const InputField: React.FC<props> = ({todo, setTodo}) => {
   return (
     <form className='input'>
         <input type='input' placeholder='Enter a task' className='input__box' />
